922b179bed5d86e3c5f22dbf7285eca32d6adb59
Roald Hacquebord <roald@zielma.kr>
2023-12-01 10:31:58 +0100

    The amount of boilerplate is insane

---
+++ day1/day1.test.ts
@@ -0,0 +1,5 @@
+describe('day1', () => {
+  it('works', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ jest.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.(spec|test)\\.ts$',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
